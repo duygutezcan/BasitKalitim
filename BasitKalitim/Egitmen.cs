4e6be9d0080d4f8c7ea4dbcbf4c53039b2943ae2
SEM <SEM@DESKTOP-4GKM6HK>
2022-11-08 09:25:41 +0300

    Add project files.

---
+++ BasitKalitim/Egitmen.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasitKalitim
+{
+    internal class Egitmen:Temel
+    {
+        public decimal Maas { get; set; }
+        public string AkademikUnvan { get; set; }
+    }
+}
---
+++ BasitKalitim/Program.cs
@@ -0,0 +1,24 @@
+﻿using BasitKalitim;
+
+Ogrenci o= new Ogrenci();
+o.Ad="Duygu";
+o.Soyad = "Tezcan Kantar";
+o.Ilce = "Ataşehir";
+o.Sehir = "İstanbul";
+o.Cadde = "Atatürk";
+o.Sokak = "Mevlana";
+
+
+Console.WriteLine(o.FullName());
+foreach (var item in o.AdresAl())
+{
+    Console.WriteLine(item);
+}
+
+Egitmen e=new Egitmen();
+e.Ad = "Şamil";
+e.Soyad = "Yılmaz";
+e.Ilce = "Ümraniye";
+e.Sehir = "İstanbul";
+e.Cadde = "Atatürk";
+
